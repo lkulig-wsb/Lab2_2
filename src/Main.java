a28ca6d0f6476e154d1575701c056072f64fe281
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-11-02 14:28:41 +0100

    Third commit.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import Package1.Client;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
@@ -12,7 +10,8 @@ public class Main {
         Client c2 = new Client(233,"GGC","John Brown St.111",
                 "Liverpool","UK","ggc@ggcgroup.uk");
 
-        Client c3 = new Client(112,"FactoryJ","Las Frycowski 1", "FrycowaForest","Poland","factory@j.com.pl");
+        Client c3 = new Client(112,"FactoryJ","Las Frycowski 1",
+                "FrycowaForest","Poland","factory@j.com.pl");
 
     //Wyświetlenie wcześniej utworzonych obiektów za pomocą metody print().
     c1.print();
