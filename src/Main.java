d2833b98ed706000a2efdbe36f466e1fc346ccbc
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-11-02 14:22:31 +0100

    Second commit.

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@ import Package1.Client;
 public class Main {
     public static void main(String[] args) {
 
-    Client c1 = new Client(345,"FKL Holdings");
+     System.out.println("=== KARTOTEKA KLIENTA ===\n");
+    //Poniżej obiekty klasy Client.
+        Client c1 = new Client(345,"FKL Holdings","St. Joseph Av. 153",
+            "London","GB","fkl@holdings.uk");
+        Client c2 = new Client(233,"GGC","John Brown St.111",
+                "Liverpool","UK","ggc@ggcgroup.uk");
+
+        Client c3 = new Client(112,"FactoryJ","Las Frycowski 1", "FrycowaForest","Poland","factory@j.com.pl");
+
+    //Wyświetlenie wcześniej utworzonych obiektów za pomocą metody print().
     c1.print();
+    c2.print();
+    c3.print();
 
     }
 }
--- src/Package1/Client.java
+++ src/Package1/Client.java
@@ -1,17 +1,37 @@
 package Package1;
-
 public class Client {
-    int clientID;
-    String clientName;
 
-    public Client (int clientID, String clientName)
+    //Pola(cechy) klasy Client. Modyfikator private ogranicza dostęp do danego pola tylko do jego własnej klasy.
+    // do element
+    private int clientID;
+    private String clientName;
+    private String clientAddress;
+    private String clientCity;
+    private String clientCountry;
+    private String emailAddress;
+
+    //Konstruktor klasy Client - inicjalizuje wartości pól obiektu.
+    public Client (int clientID, String clientName, String clientAddress,String clientCity,
+                   String clientCountry, String emailAddress)
     {
         this.clientID = clientID;
         this.clientName = clientName;
+        this.clientAddress = clientAddress;
+        this.clientCity = clientCity;
+        this.clientCountry = clientCountry;
+        this.emailAddress = emailAddress;
     }
+    //Metoda o nazwie print(). Void oznacza że metoda nie zwraca żadnej wartości.
+    // Metoda wykonuje operację polegającą na wyświetleniu danych obiektu w określonym formacie.
+    //Modyfikator public określa że jest ona dostępna z poziomu dowolnej innej klasy.
     public void print()
     {
         System.out.println("Client ID: " + clientID);
-        System.out.println("Client name: " + clientName);
+        System.out.println("Client Name: " + clientName);
+        System.out.println("Address: " + clientAddress);
+        System.out.println("City: " + clientCity);
+        System.out.println("Country: " + clientCountry);
+        System.out.println("Email: " + emailAddress);
+        System.out.println("--------------------------------------------");
     }
 }
